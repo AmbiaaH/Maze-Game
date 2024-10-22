b49601af1ae3802a8c082481a8ac0799f593390f
Rares-Gabriel Balcau <ec22514@qmul.ac.uk>
2024-10-22 19:58:39 +0100

    Changed maze design, made sure everything works, changed values so we can use the air element

--- Assets/Scripts/ReloadScene.cs
+++ Assets/Scripts/ReloadScene.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class ReloadScene : MonoBehaviour
 {
+    // OnTriggerEnter is called when another object enters the collider of the attached object
     private void OnTriggerEnter(Collider lava)
     {
+        // Check if the colliding object has the tag "Player"
         if (lava.CompareTag("Player"))
         {
-            // Save the current scene index to PlayerPrefs
+            // Save the current scene index to PlayerPrefs to reload later
             int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
             PlayerPrefs.SetInt("LastLevel", currentSceneIndex);
 
